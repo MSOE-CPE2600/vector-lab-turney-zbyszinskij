5d19c628894e288390d1105dddddd6200f62a29d
James Zbyszinski <zbyszinskij@msoe.edu>
2025-10-13 23:46:55 -0500

    Documentation for functions added

--- vector.h
+++ vector.h
@@ -18,11 +18,22 @@ typedef struct {
   float z;
 } vector;
 
+// Creates a new vector
 vector* new_vect(vector *save, char name[], float x, float y, float z);
+
+// Vector addition
 vector add(vector *a, vector *b);
+
+// Vector subtraction
 vector sub(vector *a, vector *b);
+
+// Scaler multiplication of a vector
 vector scale_mult(float x, vector *vect);
+
+// Dot product of two vectors
 float dot_prod(vector *a, vector *b);
+
+// Cross product of two vectors
 vector cross_prod(vector *a, vector *b);
 
 
